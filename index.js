1eefbe011d9f9e63137ecb61ca9c98c252b1bdfe
NPVallejos <nvallejos123@gmail.com>
2020-12-06 22:36:21 -0500

    Added adjustable volume slider
    The volume level will also be displayed above the volume slider.

--- index.js
+++ index.js
@@ -10,38 +10,61 @@ let audioElements = [
 ];
 
 let buttonElements = document.querySelectorAll('.audio-button');
-// Add event listeners for each button
-for (let i = 0; i < buttonElements.length; i++) {
-    buttonElements[i].addEventListener("click", () => {
-        switch (i) {
-            case 0:
-                playAudio(0);
-                break;
-            case 1:
-                playAudio(1);
-                break;
-            case 2:
-                playAudio(2);
-                break;
-            case 3:
-                playAudio(3);
-                break;
-            case 4:
-                playAudio(4);
-                break;
-            case 5:
-                playAudio(5);
-                break;
-            case 6:
-                playAudio(6);
-                break;
-            case 7:
-                playAudio(7);
-                break;
-            default:
-                break;
-        }
-    });
+let volumeSlider = document.getElementById('volume-control');
+let volumeDisplay = document.getElementById('volume-display');
+let volumeLevel = volumeSlider.value / 100;
+
+
+start();
+
+function start() {
+    initializeButtons();
+    setVolumeDisplay();
+}
+
+function setVolumeDisplay() {
+    volumeDisplay.innerHTML = "Volume: " + volumeLevel;
+}
+
+volumeSlider.addEventListener("input", (e) => {
+    volumeLevel = e.currentTarget.value / 100;
+    setVolumeDisplay();
+});
+
+function initializeButtons() {
+    // Add event listeners for each button
+    for (let i = 0; i < buttonElements.length; i++) {
+        buttonElements[i].addEventListener("click", () => {
+            switch (i) {
+                case 0:
+                    playAudio(0);
+                    break;
+                case 1:
+                    playAudio(1);
+                    break;
+                case 2:
+                    playAudio(2);
+                    break;
+                case 3:
+                    playAudio(3);
+                    break;
+                case 4:
+                    playAudio(4);
+                    break;
+                case 5:
+                    playAudio(5);
+                    break;
+                case 6:
+                    playAudio(6);
+                    break;
+                case 7:
+                    playAudio(7);
+                    break;
+                default:
+                    break;
+            }
+        });
+    }
 }
 
 // Important to make this async so audio can be overlapped
@@ -52,11 +75,14 @@ async function playAudio(index) {
             // 'clip' is already playing
             // Overlap the audio by creating a new Audio element
             let copy = new Audio(clip.src);
+            copy.volume = volumeLevel;
             await copy.play();
             copy = undefined;
         }
-        else
+        else {
+            clip.volume = volumeLevel;
             await clip.play();
+        }
     } catch (err) {
         console.log('Failed to play...' + err);
     }
